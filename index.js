d9ab611b7533e5dfae0c710ec1c3a489406ccd7e
harshit sampat <harshit.sampat@mindinventory.com>
2024-04-01 18:47:57 +0530

    add : add tutorial curd

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const connectDB = require("./db/db");
 const cors = require("cors");
 const verifyToken = require("./api/verifytoken");
 const cookieParser = require("cookie-parser");
+const tutorialController = require("./api/tutorial");
 require("dotenv").config();
 const app = express();
 const port = 5000;
@@ -20,6 +21,14 @@ app.use(express.json());
 app.use("/register", register);
 app.use("/login", login);
 app.use("/verifytoken", verifyToken);
+
+app.get("/tutorial", tutorialController.findAll);
+app.get("/tutorial/:id", tutorialController.findOne);
+app.post("/tutorial", tutorialController.create);
+app.put("/tutorial/:id", tutorialController.update);
+app.delete("/tutorial/:id", tutorialController.remove);
+app.delete("/tutorial", tutorialController.removeAll);
+app.get("/tutorial/published", tutorialController.findAllPublished);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
